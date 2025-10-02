9e01c6a2ed318ee12c036ee34daee08b06165f86
Zachary Tucker <ztucker99@gmail.com>
2025-10-02 10:36:16 -0400

    script update

--- web115/scripts/scripts.js
+++ web115/scripts/scripts.js
@@ -2,13 +2,14 @@ function greeting() {
     const firstName = document.getElementById('first_name').value;
     const lastName = document.getElementById('last_name').value;
     const middleInitial = document.getElementById('middle_initial').value;
+
     let greeting = `Welcome to TuckTech ${firstName}`;
 
-    if (middle !== "") {
-        greeting += middleInitial + ".";
+    if (middleInitial !== "") {
+        greeting += " " + middleInitial + ".";
     }
 
-    greeting += lastName + "!";
+    greeting += " " + lastName + "!";
 
-    document.getElementById("greeting").textContent = `${greeting}`;
+    document.getElementById("greeting").textContent = greeting;
 }
