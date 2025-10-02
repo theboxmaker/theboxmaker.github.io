8a074a80ce432a687b7b2e276f15d6712ad9ea1f
Zachary Tucker <ztucker99@gmail.com>
2025-10-02 11:04:54 -0400

    loop attempt 1

--- web115/scripts/scripts.js
+++ web115/scripts/scripts.js
@@ -12,4 +12,25 @@ function greeting() {
     greeting += " " + lastName + "!";
 
     document.getElementById("greeting").textContent = greeting;
+
+    let userNum = prompt(`How high would you like to count, ${firstName}?`);
+    userNum = parseInt(userNum);
+
+    if (!isNaN(userNum) && userNum > 0) {
+        const loopMsg = "Tucker Technology";
+        let loopRun = "";
+
+        for ( i = 1; i <= userNum; i++ ) {
+            if ( i % 2 === 0) {
+                loopRun += `${i}.) ${loopMsg} - this number is even<br>`;
+            } else {
+                loopRun += `${i}.) ${loopMsg} - this number is odd<br>`;
+            }
+        }
+
+        document.getElementById('loop').innerHTML = loopRun
+    } else {
+        document.getElementById('loop').innerHTML = "<p>Please enter a valid positive number.</p>"
+    }
+
 }
