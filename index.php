854e7ef22f4cbb3d0dfbbda0748fa90d055681b3
Zachary Tucker <ztucker99@gmail.com>
2025-10-17 17:01:04 -0400

    add index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="web250/styles/default.css">
+    <title>Zachary Tucker's Web Projects - Home</title>
+    <script src="https://lint.page/kit/67ff88.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <?php include 'web250/components/header.html'; ?>
+    <main>
+        <h2>Home</h2>
+        <p>Hello! I am Zachary Tucker, a 26 year old student learning web development. My goal is to complete my
+            schooling
+            and
+            enter the workforce. I like to spend my free time outside, either rock climbing for golfing! This site will
+            be home to any and all projects I work on for the foreseeable future!
+        </p>
+        <a href="web250/">Click to go to my WEB250 class</a>
+        <p>You can learn more about me by visiting my <a href="web250/introduction.html">introduction.</a></p>
+    </main>
+    <?php include 'web250/components/footer.html'; ?>
+</body>
+
+</html>
